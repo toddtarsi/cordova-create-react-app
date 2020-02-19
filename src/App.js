d49885d735ff974a2de8793d6d4fe982ffbf3201
toddtarsi <tarsitodd@gmail.com>
2020-02-19 14:07:36 -0600

    error boundary

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidCatch(err, info) {
+    this.props.Sentry.captureException(err);
+  }
   render() {
     return (
       <div className="App">
