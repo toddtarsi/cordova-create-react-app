57db250877c367b6df106155fff375859154e440
toddtarsi <tarsitodd@gmail.com>
2020-02-19 14:03:28 -0600

    sentry init call

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Sentry from '@sentry/browser';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,6 +6,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const startApp = () => {
+  Sentry.init('WHATEVER_GOES_HERE');
   ReactDOM.render(<App />, document.getElementById('root'));
   registerServiceWorker();
 };
